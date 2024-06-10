f4e9a06a3406af0b18cb875a862e34c37d1a728d
Manikandan <manikumar31520@gmail.com>
2024-06-10 17:06:54 +0530

    new codes added

--- server.ts
+++ server.ts
@@ -1,48 +1,42 @@
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 import express from "express";
 import axios from "axios";
-import * as fs from "fs";
+import fs from "fs";
 import path from "path";
 import cors from "cors";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 const { WEBHOOK_VERIFY_TOKEN, GRAPH_API_TOKEN, PORT } = process.env;
 
-// Ensure the images directory exists
+// Ensure the directories exist
+const ensureDirExists = (dir: any) => {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+};
+
 const imagesDir = path.resolve('images');
-if (!fs.existsSync(imagesDir)) {
-  fs.mkdirSync(imagesDir);
-}
+const documentsDir = path.resolve('documents');
+const audioDir = path.resolve('audio');
+const videosDir = path.resolve('videos');
 
-
-// Ensure the document directory exists
-const documentDir = path.resolve('documents');
-if (!fs.existsSync(documentDir)) {
-  fs.mkdirSync(documentDir);
-}
+ensureDirExists(imagesDir);
+ensureDirExists(documentsDir);
+ensureDirExists(audioDir);
+ensureDirExists(videosDir);
 
 // Simple in-memory cache for processed message IDs
 const processedMessages = new Set();
 
 app.post("/webhook", async (req, res) => {
-  // log incoming messages
   console.log("Incoming webhook message:", JSON.stringify(req.body, null, 2));
 
-  // check if the webhook request contains a message
-  // details on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
-  const message = req.body.entry?.[0]?.changes[0]?.value?.messages?.[0];
-
+  const message = req.body.entry?.[0]?.changes?.[0]?.value?.messages?.[0];
   if (!message) {
     return res.sendStatus(200);
   }
@@ -62,20 +56,8 @@ app.post("/webhook", async (req, res) => {
   //   processedMessages.delete(ids[0]);
   // }
 
-  // axios(postItem)
-  //   .then(response => {
-  //     console.log('Response:', response.data);
-  //   })
-  //   .catch(error => {
-  //     console.error('Error:', error);
-  //   });
-
-  // check if the incoming message contains text
   if (message?.type === "text") {
-    // extract the business number to send the reply from it
-    const business_phone_number_id =
-      req.body.entry?.[0].changes?.[0].value?.metadata?.phone_number_id;
-
+    const businessPhoneNumberId = req.body.entry?.[0]?.changes?.[0]?.value?.metadata?.phone_number_id;
     const postItem = {
       url: "https://prod-134.westus.logic.azure.com:443/workflows/54a65ff633684999b86c7d2067a4ed82/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=KNvJymBCdH3oU-BzKHvXT3BXmWju_IzMyCNkCevuEwE",
       method: "POST",
@@ -97,25 +79,32 @@ app.post("/webhook", async (req, res) => {
     } catch (error) {
       console.error('Error sending data to Azure Logic App:', error);
     }
-    // mark incoming message as read
-    await axios({
-      method: "POST",
-      url: `https://graph.facebook.com/v18.0/${business_phone_number_id}/messages`,
-      headers: {
-        Authorization: `Bearer ${GRAPH_API_TOKEN}`,
-      },
-      data: {
-        messaging_product: "whatsapp",
-        status: "read",
-        message_id: message.id,
-      },
-    });
+    try {
+      await axios({
+        method: "POST",
+        url: `https://graph.facebook.com/v18.0/${businessPhoneNumberId}/messages`,
+        headers: {
+          Authorization: `Bearer ${GRAPH_API_TOKEN}`,
+        },
+        data: {
+          messaging_product: "whatsapp",
+          status: "read",
+          message_id: message.id,
+        },
+      });
+
+      return res.sendStatus(200);
+    } catch (error: any) {
+      console.error('Error marking message as read:', error.response ? error.response.data : error.message);
+      return res.status(500).send('Error processing message');
+    }
   }
-  else if (message?.type === "image") {
+
+  if (message?.type === "image") {
     const imageId = message?.image.id;
+    const phoneNumberId = req.body.entry?.[0]?.changes?.[0]?.value?.metadata?.phone_number_id;
 
     try {
-      // Step 1: Get the image metadata including the URL
       const imageMetaResponse = await axios({
         url: `https://graph.facebook.com/v13.0/${imageId}`,
         method: 'GET',
@@ -127,8 +116,6 @@ app.post("/webhook", async (req, res) => {
       const imageUrl = imageMetaResponse.data.url;
       console.log(`Image URL: ${imageUrl}`);
 
-
-      // Step 2: Fetch the image data
       const imageResponse = await axios({
         url: imageUrl,
         method: 'GET',
@@ -138,12 +125,12 @@ app.post("/webhook", async (req, res) => {
         },
       });
 
-      // Step 3: Save the image to the server
       const imagePath = path.resolve(imagesDir, `${imageId}.jpg`);
       console.log(`Absolute path for saving image: ${imagePath}`);
 
       fs.writeFileSync(imagePath, imageResponse.data);
       console.log("Image response data length:", imageResponse.data.byteLength);
+
       const imageItem = {
         url: "https://prod-134.westus.logic.azure.com:443/workflows/54a65ff633684999b86c7d2067a4ed82/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=KNvJymBCdH3oU-BzKHvXT3BXmWju_IzMyCNkCevuEwE",
         method: "POST",
@@ -159,6 +146,7 @@ app.post("/webhook", async (req, res) => {
           Name: req.body.entry?.[0]?.changes[0]?.value?.contacts?.[0].profile.name
         }
       };
+
       try {
         const response = await axios(imageItem);
         console.log('Response from Azure Logic App:', response.data);
@@ -166,8 +154,6 @@ app.post("/webhook", async (req, res) => {
         console.error('Error sending data to Azure Logic App:', error);
       }
 
-
-      // Verify the file exists
       if (fs.existsSync(imagePath)) {
         console.log(`Image successfully saved as ${imageId}.jpg at ${imagePath}`);
       } else {
@@ -181,8 +167,10 @@ app.post("/webhook", async (req, res) => {
       return res.status(500).send('Error retrieving image');
     }
   }
-  else if (message?.type === "document") {
+
+  if (message?.type === "document") {
     const documentId = message?.document.id;
+    const phoneNumberId = req.body.entry?.[0]?.changes?.[0]?.value?.metadata?.phone_number_id;
 
     try {
       const documentMetaResponse = await axios({
@@ -196,7 +184,6 @@ app.post("/webhook", async (req, res) => {
       const documentUrl = documentMetaResponse.data.url;
       console.log(`Document URL: ${documentUrl}`);
 
-
       const documentResponse = await axios({
         url: documentUrl,
         method: 'GET',
@@ -207,32 +194,11 @@ app.post("/webhook", async (req, res) => {
       });
 
       const documentName = message.document.filename || `${documentId}.pdf`;
-      const documentPath = path.resolve(documentDir, documentName);
+      const documentPath = path.resolve(documentsDir, documentName);
       console.log(`Absolute path for saving document: ${documentPath}`);
 
       fs.writeFileSync(documentPath, documentResponse.data);
       console.log("Document response data length:", documentResponse.data.byteLength);
-      const documentItem = {
-        url: "https://prod-134.westus.logic.azure.com:443/workflows/54a65ff633684999b86c7d2067a4ed82/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=KNvJymBCdH3oU-BzKHvXT3BXmWju_IzMyCNkCevuEwE",
-        method: "POST",
-        timeout: 0,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          "Accept": "application/json; odata=nometadata",
-          "Content-Type": "application/json; odata=nometadata"
-        },
-        data: {
-          DocumentURL: `https://webhook.remodigital.in/documents/${documentId}.pdf`,
-          Number: req.body.entry?.[0]?.changes[0]?.value?.messages?.[0].from,
-          Name: req.body.entry?.[0]?.changes[0]?.value?.contacts?.[0].profile.name
-        }
-      };
-      try {
-        const response = await axios(documentItem);
-        console.log('Response from Azure Logic App:', response.data);
-      } catch (error) {
-        console.error('Error sending data to Azure Logic App:', error);
-      }
 
       if (fs.existsSync(documentPath)) {
         console.log(`Document successfully saved as ${documentName} at ${documentPath}`);
@@ -245,6 +211,96 @@ app.post("/webhook", async (req, res) => {
     } catch (error: any) {
       console.error('Error retrieving document:', error.response ? error.response.data : error.message);
       return res.status(500).send('Error retrieving document');
+    }
+  }
+
+  if (message?.type === "audio") {
+    const audioId = message?.audio.id;
+    const phoneNumberId = req.body.entry?.[0]?.changes?.[0]?.value?.metadata?.phone_number_id;
+
+    try {
+      const audioMetaResponse = await axios({
+        url: `https://graph.facebook.com/v13.0/${audioId}`,
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${GRAPH_API_TOKEN}`,
+        },
+      });
+
+      const audioUrl = audioMetaResponse.data.url;
+      console.log(`Audio URL: ${audioUrl}`);
+
+      const audioResponse = await axios({
+        url: audioUrl,
+        method: 'GET',
+        responseType: 'arraybuffer',
+        headers: {
+          'Authorization': `Bearer ${GRAPH_API_TOKEN}`,
+        },
+      });
+
+      const audioPath = path.resolve(audioDir, `${audioId}.ogg`);
+      console.log(`Absolute path for saving audio: ${audioPath}`);
+
+      fs.writeFileSync(audioPath, audioResponse.data);
+      console.log("Audio response data length:", audioResponse.data.byteLength);
+
+      if (fs.existsSync(audioPath)) {
+        console.log(`Audio successfully saved as ${audioId}.ogg at ${audioPath}`);
+      } else {
+        console.error(`Failed to save audio at ${audioPath}`);
+        return res.status(500).send('Failed to save audio');
+      }
+
+      return res.status(200).send('Audio retrieved successfully');
+    } catch (error: any) {
+      console.error('Error retrieving audio:', error.response ? error.response.data : error.message);
+      return res.status(500).send('Error retrieving audio');
+    }
+  }
+
+  if (message?.type === "video") {
+    const videoId = message?.video.id;
+    const phoneNumberId = req.body.entry?.[0]?.changes?.[0]?.value?.metadata?.phone_number_id;
+
+    try {
+      const videoMetaResponse = await axios({
+        url: `https://graph.facebook.com/v13.0/${videoId}`,
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${GRAPH_API_TOKEN}`,
+        },
+      });
+
+      const videoUrl = videoMetaResponse.data.url;
+      console.log(`Video URL: ${videoUrl}`);
+
+      const videoResponse = await axios({
+        url: videoUrl,
+        method: 'GET',
+        responseType: 'arraybuffer',
+        headers: {
+          'Authorization': `Bearer ${GRAPH_API_TOKEN}`,
+        },
+      });
+
+      const videoPath = path.resolve(videosDir, `${videoId}.mp4`);
+      console.log(`Absolute path for saving video: ${videoPath}`);
+
+      fs.writeFileSync(videoPath, videoResponse.data);
+      console.log("Video response data length:", videoResponse.data.byteLength);
+
+      if (fs.existsSync(videoPath)) {
+        console.log(`Video successfully saved as ${videoId}.mp4 at ${videoPath}`);
+      } else {
+        console.error(`Failed to save video at ${videoPath}`);
+        return res.status(500).send('Failed to save video');
+      }
+
+      return res.status(200).send('Video retrieved successfully');
+    } catch (error: any) {
+      console.error('Error retrieving video:', error.response ? error.response.data : error.message);
+      return res.status(500).send('Error retrieving video');
     }
   }
 
@@ -274,7 +330,7 @@ app.get('/images/:mediaId', (req, res) => {
 
 app.get('/documents/:documentName', (req, res) => {
   const documentName = req.params.documentName;
-  const documentPath = path.resolve(documentDir, documentName);
+  const documentPath = path.resolve(documentsDir, documentName);
 
   console.log('Request for document with name:', documentName);
   console.log('Looking for document at path:', documentPath);
@@ -293,31 +349,65 @@ app.get('/documents/:documentName', (req, res) => {
   }
 });
 
-// accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
-// info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests
+app.get('/audio/:audioId', (req, res) => {
+  const audioId = req.params.audioId;
+  const audioPath = path.resolve(audioDir, `${audioId}.ogg`);
+
+  console.log('Request for audio with audioId:', audioId);
+  console.log('Looking for audio at path:', audioPath);
+
+  if (fs.existsSync(audioPath)) {
+    console.log(`Serving audio from path: ${audioPath}`);
+    res.sendFile(audioPath, { headers: { 'Content-Type': 'audio/ogg' } }, (err: any) => {
+      if (err) {
+        console.error('Error sending file:', err);
+        res.status(err.status).end();
+      }
+    });
+  } else {
+    console.error('Audio not found at path:', audioPath);
+    res.status(404).send('Audio not found');
+  }
+});
+
+app.get('/videos/:videoId', (req, res) => {
+  const videoId = req.params.videoId;
+  const videoPath = path.resolve(videosDir, `${videoId}.mp4`);
+
+  console.log('Request for video with videoId:', videoId);
+  console.log('Looking for video at path:', videoPath);
+
+  if (fs.existsSync(videoPath)) {
+    console.log(`Serving video from path: ${videoPath}`);
+    res.sendFile(videoPath, { headers: { 'Content-Type': 'video/mp4' } }, (err: any) => {
+      if (err) {
+        console.error('Error sending file:', err);
+        res.status(err.status).end();
+      }
+    });
+  } else {
+    console.error('Video not found at path:', videoPath);
+    res.status(404).send('Video not found');
+  }
+});
+
+// Webhook verification
 app.get("/webhook", (req, res) => {
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
 
-  // check the mode and token sent are correct
   if (mode === "subscribe" && token === WEBHOOK_VERIFY_TOKEN) {
-    // respond with 200 OK and challenge token from the request
     res.status(200).send(challenge);
     console.log("Webhook verified successfully!");
   } else {
-    // respond with '403 Forbidden' if verify tokens do not match
     res.sendStatus(403);
   }
 });
 
-// app.get("/", (req, res) => {
-//   res.send(`<pre>Nothing to see here.
-// Checkout README.md to start.</pre>`);
-// });
-app.get('*', (req, res) => {
-  res.send("API is hosted")
-})
+app.get("/", (req, res) => {
+  res.send(`<pre>Nothing to see here. Checkout README.md to start.</pre>`);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`);
